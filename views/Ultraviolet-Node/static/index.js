92ffbe244eccbf3d0c69e78d0269e1a762bbb958
M0rgits <93285887+M0rgits@users.noreply.github.com>
2022-09-19 14:45:56 +0000

    Fixed links page

--- views/Ultraviolet-Node/static/index.js
+++ views/Ultraviolet-Node/static/index.js
@@ -1,8 +1,65 @@
 const form = document.querySelector('form');
 const geforce = document.getElementById("geforce");
 const cmgames = document.getElementById("cmgames");
+const ytlnk = document.getElementById('ytlnk');
+const dclnk = document.getElementById('dclnk');
+const ntlnk = document.getElementById('ntlnk');
+const rdlnk = document.getElementById('rdlnk');
 
+rdlnk.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = "http://reddit.com";
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
 
+ntlnk.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = "http://netflix.com";
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+dclnk.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = "http://discordapp.com";
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+ytlnk.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = "http://youtube.com";
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+ytlnk.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = "http://youtube.com";
+        
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
 
 geforce.addEventListener('click', async event => {
     event.preventDefault();
